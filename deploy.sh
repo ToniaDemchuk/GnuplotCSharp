44d212239efc5a2a4b9eda54ea5abbb280bd9289
Tonia Demchuk <tonia.kolkevych@gmail.com>
2018-01-29 00:15:03 +0200

    add artifacts forlder

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ Version=$1
 Source=$2
 ApiKey=$3
 
-dotnet pack --no-build --no-restore --include-symbols -c Release /p:Version=$Version
-dotnet nuget push *.nupkg -k $ApiKey -s $Source
+mkdir nuget_packages
+dotnet pack -o nuget_packages --no-build --no-restore --include-symbols -c Release /p:Version=$Version
+dotnet nuget push ./nuget_packages/*.nupkg -k $ApiKey -s $Source
