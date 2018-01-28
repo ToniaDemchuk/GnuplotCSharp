ae494e30ec63d0a0307f8e8dc677d595459f7b95
Tonia Demchuk <tonia.kolkevych@gmail.com>
2018-01-29 00:06:27 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ Source=$2
 ApiKey=$3
 
 dotnet pack --no-build --no-restore --include-symbols -c Release /p:Version=$Version
-dotnet nuget push ./*.nupkg -k $ApiKey -s $Source
+dotnet nuget push *.nupkg -k $ApiKey -s $Source
